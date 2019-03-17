fb61a7410c5b6800ac2e465db8cca329edc069b1
Thomas Read <thjread@gmail.com>
2019-03-17 12:08:30 +0000

    Configuration

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -1,4 +1,5 @@
 const CopyWebpackPlugin = require("copy-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 const path = require('path');
 
 module.exports = {
@@ -7,8 +8,8 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "bootstrap.js",
   },
-  mode: "development",
   plugins: [
-    new CopyWebpackPlugin(['index.html'])
-  ],
+      new CleanWebpackPlugin(),
+      new CopyWebpackPlugin(['index.html', 'index.css', '404.html', '50x.html', 'favicon.ico', 'firasans-light-webfont.woff', 'firasans-light-webfont.woff2', 'robots.txt', {from: 'pdfs/cv.pdf', to: 'pdfs/cv.pdf'}])
+  ]
 };
