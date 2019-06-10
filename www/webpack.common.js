965250a138c4d4327fb5ea9333db47ad5123741c
Thomas Read <thjread@gmail.com>
2019-06-10 16:59:07 +0100

    Update dependencies

--- www/webpack.common.js
+++ www/webpack.common.js
@@ -1,5 +1,5 @@
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const PreloadWebpackPlugin = require('preload-webpack-plugin');
